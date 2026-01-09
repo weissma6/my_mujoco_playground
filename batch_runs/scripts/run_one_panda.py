a1a1b7ac2c207962133575dfd088190783ebd77e
weissma6 <weissma6@students.zhaw.ch>
2026-01-09 12:56:18 +0100

    Change the run_:one_panda back to panda test

--- batch_runs/scripts/run_one_panda.py
+++ batch_runs/scripts/run_one_panda.py
@@ -1,7 +1,7 @@
 import argparse
 import json
 import os
-from learning.notebooks.Panda_ppo import run_experiment
+from learning.notebooks.panda_test import run_experiment
 
 
 def load_config(jsonl_path: str, index_1based: int) -> dict:
