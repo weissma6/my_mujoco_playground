b1160c7b5cc108438947257a9ec527320792b7c5
weissma6 <weissma6@students.zhaw.ch>
2026-01-16 20:38:28 +0100

    remove resolution from ur10_:ppo

--- learning/notebooks/UR10_ppo.py
+++ learning/notebooks/UR10_ppo.py
@@ -179,12 +179,7 @@ def _rollout_and_log_video_from_make_policy(
     trajectory = rollout[:: int(render_every)]
     print("camera kwargs:", camera_kwargs, flush=True)
     cam = camera_kwargs or {"camera": "side_130"}
-    cam = {
-    "camera": "side_130",
-    "width": 960,
-    "height": 720,
-    }
-    print("camera kwargs for rendering:", cam, flush=True)
+    print("cam for rendering:", cam, flush=True)
 
     frames = eval_env.render(trajectory, **cam)
     # frames might be list-of-arrays; make it (T,H,W,C) uint8 on CPU
