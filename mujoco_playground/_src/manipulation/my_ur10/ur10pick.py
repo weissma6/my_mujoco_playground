907c8ae04f5503b8299229e1bb3c80dc093de880
weissma6 <weissma6@students.zhaw.ch>
2026-01-18 15:52:49 +0100

    no info out in logging

--- mujoco_playground/_src/manipulation/my_ur10/ur10pick.py
+++ mujoco_playground/_src/manipulation/my_ur10/ur10pick.py
@@ -395,7 +395,7 @@ class UR10PickCube(ur10_base.UR10Base):
             "number_floor_collision": floor_collision,
         }
 
-        return rewards, raw, info
+        return rewards, raw
 
     def _get_obs(self, data: mjx.Data, info: dict[str, Any]) -> jax.Array:
         gripper_pos = data.site_xpos[self._gripper_site]
