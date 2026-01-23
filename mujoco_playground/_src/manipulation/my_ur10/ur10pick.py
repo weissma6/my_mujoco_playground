2ee5314a85581f7de9724fabe40ae61729540164
weissma6 <weissma6@students.zhaw.ch>
2026-01-23 15:30:18 +0100

    Box init random.

--- mujoco_playground/_src/manipulation/my_ur10/ur10pick.py
+++ mujoco_playground/_src/manipulation/my_ur10/ur10pick.py
@@ -118,15 +118,15 @@ class UR10PickCube(ur10_base.UR10Base):
         rng, rng_box, rng_target = jax.random.split(rng, 3)
 
         # initialize box position
-        # box_pos = (
-        #     jax.random.uniform(
-        #         rng_box,
-        #         (3,),
-        #         minval=jp.array([-0.2, -0.2, 0.0]),
-        #         maxval=jp.array([0.2, 0.2, 0.0]),
-        #     )
-        #     + self._init_obj_pos # Box position from XML Keyframe
-        # )
+        box_pos = (
+            jax.random.uniform(
+                rng_box,
+                (3,),
+                minval=jp.array([-0.2, -0.2, 0.0]),
+                maxval=jp.array([0.2, 0.2, 0.0]),
+            )
+            + self._init_obj_pos # Box position from XML Keyframe
+        )
         # initialize box position without randomnes
         box_pos = self._init_obj_pos
 
@@ -135,8 +135,8 @@ class UR10PickCube(ur10_base.UR10Base):
             jax.random.uniform(
                 rng_target,
                 (3,),
-                minval=jp.array([-0.2, -0.6, 0]),
-                maxval=jp.array([0.2, -0.4, 0]),
+                minval=jp.array([-0.2, -0.6, 0.4]),
+                maxval=jp.array([0.2, -0.4, 0.6]),
             )
             + self._init_obj_pos # Box position from XML Keyframe
         )
